64ba574aa6c8012494f9ea6cbf1acc2157facd9e
LukaOliveira <lukaoliveira1401@gmail.com>
2020-11-28 19:17:46 -0300

    Css and Code PHP Update

---
+++ home.php
@@ -0,0 +1,26 @@
+<?php
+
+    session_start();
+
+    if(!isset($_SESSION['logged']) || $_SESSION['level'] != 1){
+        header('Location: login.php');
+    }
+
+    require_once "database.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Olá, <?php
+    
+        echo $_SESSION['name'];
+    
+    ?>!</h1>
+</body>
+</html>
